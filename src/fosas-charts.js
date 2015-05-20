308bddefe2f4682bd692f2d4afee12ff667a238e
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2015-05-20 02:21:26 +0200

    Se arregló problema con visualizaciones repetidas en fosas. Arreglado tipo en mujeres

--- src/fosas-charts.js
+++ src/fosas-charts.js
@@ -1,25 +1,27 @@
 $(document).ready(
 		function() {
 			var sql = new cartodb.SQL({user:'vidascontadas'});
+			var svg_prov, svg_fosa;
 			$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-				main = document.getElementById('fosas_actuacion');
-				viewportWidth = (Math.min(main.clientWidth,window.innerWidth) || 500);
-				/* Provincias */
+				var main = document.getElementById('fosas_actuacion');
+				var viewportWidth = (Math.min(main.clientWidth,window.innerWidth) || 500);
 				var width = viewportWidth * 0.8, 
 				height = viewportWidth * 0.7,
 				format = d3.format(",d"), 
 				color = d3.scale.category20c();
-
-				$('.chart svg').width(width).height(height);
 				var bubble = d3.layout.pack().sort(null).size(
 						[ width, height]).padding(1.5);
 
-				var svg_prov = d3.select("#provincia")
+				$('.chart svg').width(width).height(height);
+				
+				if(svg_prov == undefined){
+					/* Provincias */
+					svg_prov = d3.select("#provincia")
 					.append("svg").attr("width", width)
 					.attr("height", height)
 					.attr("class", "bubble");
 
-				sql.execute("SELECT provincia,sum(victimas) total FROM fosas group by provincia order by total")
+					sql.execute("SELECT provincia,sum(victimas) total FROM fosas group by provincia order by total")
 					.done(function(data){
 						if(data.total_rows>0){
 							var total = 0;
@@ -57,53 +59,57 @@ $(document).ready(
 					.error(function(errors){
 						
 					});
-				
-				/* Tipo fosa */
-				color2 = d3.scale.category20c();
-				var svg_fosa = d3.select("#tipo-fosa")
-				.append("svg").attr("width", width)
-				.attr("height", height)
-				.attr("class", "bubble");
-
-				sql.execute("SELECT tipo,count(*) total FROM fosas group by tipo order by total")
-				.done(function(data){
-					if(data.total_rows>0){
-						var total = 0;
-						var children = []
-						$.each(data.rows,function(index,item){
-							children.push({id:"bubble-"+(index+1),name:item.tipo,value:item.total});
-						});
-						var nodes = {id:"bubble-0",children:children};
-						var node = svg_fosa.selectAll('.node')
-							.data(bubble.nodes(nodes))
-							.enter()
-							.append('g')
-								.attr('class','node')
-								.attr('id',function(d){return d.id})
-								.attr('transform',function(d){ return "translate(" + d.x + "," + d.y + ")";});
-						
-						node.append("title").text(function(d) {
-							return d.name + ": " + format(d.value);
-						});
-
-						node.append("circle").attr("r", function(d) {
-							return d.r;
-						}).style("fill", function(d) {
-							return color2(d.name);
-						});
-
-						node.append("text")
-							.attr("dy", ".3em")
-							.style("text-anchor","middle").text(function(d) {
-								text = d.name + " (" + format(d.value)+")";
-
-							return text.substring(0, d.r / 3);
-						});
-					}
-				})
-				.error(function(errors){
 					
-				});
+				}
+
+				if(svg_fosa == undefined){
+					/* Tipo fosa */
+					color2 = d3.scale.category20c();
+					svg_fosa = d3.select("#tipo-fosa")
+					.append("svg").attr("width", width)
+					.attr("height", height)
+					.attr("class", "bubble");
+
+					sql.execute("SELECT tipo,count(*) total FROM fosas group by tipo order by total")
+					.done(function(data){
+						if(data.total_rows>0){
+							var total = 0;
+							var children = []
+							$.each(data.rows,function(index,item){
+								children.push({id:"bubble-"+(index+1),name:item.tipo,value:item.total});
+							});
+							var nodes = {id:"bubble-0",children:children};
+							var node = svg_fosa.selectAll('.node')
+								.data(bubble.nodes(nodes))
+								.enter()
+								.append('g')
+									.attr('class','node')
+									.attr('id',function(d){return d.id})
+									.attr('transform',function(d){ return "translate(" + d.x + "," + d.y + ")";});
+							
+							node.append("title").text(function(d) {
+								return d.name + ": " + format(d.value);
+							});
+
+							node.append("circle").attr("r", function(d) {
+								return d.r;
+							}).style("fill", function(d) {
+								return color2(d.name);
+							});
+
+							node.append("text")
+								.attr("dy", ".3em")
+								.style("text-anchor","middle").text(function(d) {
+									text = d.name + " (" + format(d.value)+")";
+
+								return text.substring(0, d.r / 3);
+							});
+						}
+					})
+					.error(function(errors){
+						
+					});
+				}
 				
 				/* Tipo actuacion*/
 				sql.execute("SELECT promotor_actuacion as label,count(promotor_actuacion) as value from fosas group by promotor_actuacion")
